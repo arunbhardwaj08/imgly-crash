013b3465eb6083c55d30024a42638d0bf072c203
Mantu <mantu.kumar@itechnolabs.tech>
2025-06-26 15:25:52 +0530

    feat(http-client): improve error handling, dynamic auth headers, and toast notifications
    - Removed hardcoded baseURL
    - Added dynamic Authorization header injection
    - Improved request config to support JSON and FormData payloads
    - Implemented Axios response interceptor with logging and toast error notifications
    - Updated setAuthorization and clearAuthorization to use proper Authorization header casing
    - Simplified exports to named client, setAuthorization, and clearAuthorization

--- src/redux/services/HttpClient.js
+++ src/redux/services/HttpClient.js
@@ -1,7 +1,7 @@
+import { showErrorToast } from "@/components/ToastAlert";
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https:example.com",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,56 +9,63 @@ const client = axios.create({
 
 client.interceptors.request.use(
   (config) => {
-    if (config?.data instanceof FormData) {
-      // set headers for FormData
-      config.headers = {
-        ...config?.headers,
-        "Content-Type": "multipart/form-data",
-      };
-    } else {
-      // set headers for JSON data
-      config.headers = {
-        ...config?.headers,
-        "Content-Type": "application/json",
-      };
+    const token = "PUT YOUR AUTH TOKEN HERE";
+    const baseHeaders = {
+      ...config.headers,
+    };
+
+    if (token) {
+      baseHeaders.Authorization = `Bearer ${token}`;
     }
 
+    config.headers = {
+      ...baseHeaders,
+      "Content-Type":
+        config.data instanceof FormData
+          ? "multipart/form-data"
+          : "application/json",
+    };
+    // __DEV__ && console.log("Starting Request:", JSON.stringify(config, null, 2));
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-// client.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response) {
-//       // Return a standardized error format
-//       return Promise.reject({
-//         status: error.response.status,
-//         data: error.response.data,
-//       });
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       return Promise.reject({
-//         status: null,
-//         data: error.message,
-//       });
-//     } else {
-//       // Something happened in setting up the request
-//       return Promise.reject({
-//         status: null,
-//         data: error.message,
-//       });
-//     }
-//   }
-// );
+client.interceptors.response.use(
+  (response) => {
+    // __DEV__ && console.log("\n\n-----API  RESPOSNE----\n" + JSON.stringify(response) + "\n\n");
+
+    return response;
+  },
+  (error) => {
+    __DEV__ &&
+      console.log(
+        "\n\n-----API ERROR RESPOSNE----\n" +
+          JSON.stringify(error.response.data) +
+          "\n\n"
+      );
+
+    if (error.response.data) {
+      var errorMessage = "";
+      if (error.response.data.error) {
+        errorMessage = error.response.data.error.join(", ");
+      } else {
+        errorMessage = error.response.data.message;
+      }
+
+      showErrorToast({ title: errorMessage ?? "Something went wrong" });
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 const setAuthorization = (token) => {
-  client.defaults.headers.common.authorization = `Bearer ${token}`;
+  client.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearAuthorization = () => {
-  delete client.defaults.headers.common.authorization;
+  delete client.defaults.headers.common.Authorization;
 };
 
-export const HttpClient = { ...client, setAuthorization, clearAuthorization };
+export { client, setAuthorization, clearAuthorization };
